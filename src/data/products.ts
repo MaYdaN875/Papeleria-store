70c403b8d29c5b3667611783bb731148a4936ef4
MaYdaN875 <danielleon0100@gmail.com>
2026-01-18 14:50:14 -0600

    First Commit

---
+++ src/data/products.ts
@@ -0,0 +1,13 @@
+import { Product } from "../types/Product"
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: "Cuaderno profesional",
+        price: 65,
+        category: "Escolar",
+        description: "Cuaderno de 100 hojas",
+        image: "/images/cuaderno.jpg",
+        stock: 20,
+    },
+]
