67b151a55d43a8aea2c22b8b5bfb1439db42444c
MaYdaN875 <danielleon0100@gmail.com>
2026-01-19 18:16:07 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 
-
 function App() {
 
 
   return (
     <>
+      <div className="Navbar">
+        <div className="logo">
+          MyApp
+        </div>
+        <div className="links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <div className="contact">
+          <h1>Contact Us</h1>
+        </div>
+      </div>
 
     </>
   )
