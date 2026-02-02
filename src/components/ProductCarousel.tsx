d8a58e320468cfc0582478e4312a96285dd2f3d7
xabibi11 <josephxabi@gmail.com>
2026-02-02 15:49:22 -0600

    lo mismo de abajo solo que pense que los habia guardado los cambios que yo hice pero solo se cambio una linea y no lo que se debia XD

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 import type { Product } from "../types/Product"
 import type { ProductCarouselSlideConfig } from "./ProductCarouselSlide"
