3d5460fa7020ee22f922374cdb6b3b8467f33c70
MaYdaN875 <danielleon0100@gmail.com>
2026-02-19 20:17:27 -0600

    Se elimino el router.tsx y se paso todo a App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 /**
  * Punto de entrada de la aplicación.
- * Monta el árbol React con Router y estilos globales.
+ * Monta el árbol React con App (router, rutas y layout) y estilos globales.
  */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import "./styles/index.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
